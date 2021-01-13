597659e9bbcc4e9f2340a198174b248ca4e233d3
Sam Hogenbirk <sam@samwise.nl>
2021-01-13 10:48:13 +0100

    Adding les 6

---
+++ callback/script.js
@@ -0,0 +1,18 @@
+let log = console.log
+const vak = "wiskunde"
+
+function huiswerkMaken(vak, callback) {
+
+    setTimeout(() => {
+        console.log(`Ok, ok, ik ga nu mijn ${vak} huiswerk maken`)
+    }, 2000)
+
+};
+
+
+function klaarMetHuiswerk() {
+    console.log("Kijk Paps/Mams, ik ben klaar met mijn huiswerk!")
+};
+
+huiswerkMaken(vak, klaarMetHuiswerk);
+
