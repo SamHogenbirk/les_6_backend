9c540904c3075d602a5d4060132e712a347017a5
Sam Hogenbirk <sam@samwise.nl>
2021-02-01 19:55:15 +0100

    added async/await

--- Async/await/api-client.js
+++ Async/await/api-client.js
@@ -2,29 +2,104 @@
 
 const API_KEY = "817e6b531d940a0b1b0da6cea4c19fec"
 
-let getData = async () => {
+//GET movies by genre
+const getDataByGenre = async () => {
 
     const apiUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`
 
     try {
 
-        let response = await fetch(apiUrl);
-        let data = await response.json();
-        return data
+        const res = await fetch(apiUrl, { method: "GET" })
+            .then(response => response.json())
+            .then(data => data)
+        return res
+
     }
 
-    catch {
-        console.log("something went wrong")
+    catch (err) {
 
+        console.log("something went wrong" + err)
     }
 }
 
+//GET movies by rating?
+const getDataByRating = async () => {
 
+    const apiUrl = `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}`
 
+    try {
 
+        const res = await fetch(apiUrl, { method: "GET" })
 
+            .then(response => response.json())
+            .then(data => data.results)
+        return res
+    }
 
+    catch (err) {
 
+        console.log('something went wrong' + err)
+    }
+}
+
+//GET alltime favorite
+const getSingleMovie = async () => {
+
+    const apiUrl = `https://api.themoviedb.org/3/find/tt0118715?api_key=817e6b531d940a0b1b0da6cea4c19fec&language=en-US&external_source=imdb_id`
+
+    try {
+
+        const res = await fetch(apiUrl, { method: "GET" })
+
+            .then(response => response.json())
+            .then(data => data.movie_results[0])
+        return res
+    }
+
+    catch (err) {
+
+        console.log('something went wrong' + err)
+    }
+}
+
+//GET best action movies
+const getActionMovies = async () => {
+
+    const apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=1&with_genres=28`
+
+    try {
+
+        const res = await fetch(apiUrl, { method: "GET" })
+
+            .then(response => response.json())
+            .then(data => data.results)
+        return res
+    }
+
+    catch (err) {
+
+        console.log("something went wrong " + err)
+    }
+}
+
+//GET movies from 1975
+const getMovies1975 = async () => {
+
+    const apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&include_adult=true&include_video=false&page=1&sort_by=original_title.asc&primary_release_year=1975`
+
+    try {
+
+        const res = await fetch(apiUrl, { method: "GET" })
+
+            .then(response => response.json())
+            .then(data => data.results)
+        return res
+    }
+    catch (err) {
+
+        console.log("something went wrong " + err)
+    }
+}
 
 
 
--- Async/await/script.js
+++ Async/await/script.js
@@ -1,8 +1,95 @@
-async function movies() {
 
-    let result = await getData()
-    return result
+//objects of UL's
+const references = {
+
+    genreList: document.getElementById("genre-list"),
+    ratingList: document.getElementById("rating-list"),
+    favoriteList: document.getElementById("fav-list"),
+    actionList: document.getElementById("action-list"),
+    list1975: document.getElementById("1975")
 
 }
 
-console.log(movies())
+//creates list of all time favorite
+const favMovie = async () => {
+
+    await getSingleMovie()
+
+        .then(result => {
+
+            makeList("Title: ", result.title, "", "", references.favoriteList)
+
+        })
+}
+
+favMovie()
+
+//creates list of all movies by genre + ID
+const moviesGenreList = async () => {
+
+    await getDataByGenre()
+        .then(result => result.genres.forEach(element => {
+
+            // console.log(element)
+            makeList("Genre: ", element.name, "--ID: ", element.id, references.genreList)
+        }));
+
+}
+
+moviesGenreList()
+
+//creates list of top 10 movies by rating
+const movieRatingList = async () => {
+
+    const arr = []
+    await getDataByRating()
+
+        .then(result => result.filter((item) => item.title))
+        .then(data => data.forEach((item) => arr.push({ "name": item.title, "rating": item.popularity })
+        ))
+
+    arr.sort((a, b) => (a.rating < b.rating ? 1 : -1))
+    let finalList = arr.slice(0, 10)
+    finalList.forEach((element) => {
+
+        // console.log(element)
+        makeList("Title: ", element.name, "--Rating: ", element.rating, references.ratingList)
+    })
+}
+
+movieRatingList()
+
+//creates list of best action movies
+const bestActionMovies = async () => {
+
+    await getActionMovies()
+        .then(result => result.forEach((element) => {
+
+            // console.log(element)
+            makeList("Title: ", element.title, "--rating: ", element.popularity, references.actionList)
+        }));
+}
+
+bestActionMovies()
+
+const movies1975 = async () => {
+
+    await getMovies1975()
+        .then(result => result.forEach((element) => {
+
+            // console.log(element)
+            makeList("Title: ", element.title, "--Date: ", element.release_date, references.list1975)
+        }));
+}
+
+movies1975()
+
+//creates and appends LI's
+let makeList = (name, key, id, value, list) => {
+
+    const newLi = document.createElement("li")
+    newLi.innerHTML = `${name} ${key} ${id} ${value}`
+    list.appendChild(newLi)
+
+}
+
